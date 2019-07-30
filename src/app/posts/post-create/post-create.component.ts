5fa0ed2f37bc5b86c6b849d7247c0f2881ca3732
Rahul kumar <rahul.kumar@hpe.com>
2019-07-30 11:55:01 +0530

    Outputting Content by Clicking on Save Post Button and Also default content in Test Area which comes by default

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,15 @@
 import { Component } from "@angular/core";
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html"
 })
-export class PostCreateComponent {
+export class PostCreateComponent
+{
+  newPost = 'NO CONTENT';
   onAddPost() {
     alert("Post Added");
+    this.newPost = 'The user\'s Post';
   }
 }
