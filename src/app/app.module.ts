a7a7aff267c6be3bdc58fe7f6981c7800f13520b
Rahul kumar <rahul.kumar@hpe.com>
2019-07-30 15:53:06 +0530

    Added Matcard Module, Mat Button Module to enhance GUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -16,7 +19,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
 
   ],
   providers: [],
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,7 +3,8 @@ import { templateJitUrl } from "@angular/compiler";
 
 @Component({
   selector: "app-post-create",
-  templateUrl: "./post-create.component.html"
+  templateUrl: "./post-create.component.html",
+  styleUrls:['./post-create.component.css']
 })
 export class PostCreateComponent {
   newPost = "NO CONTENT";
