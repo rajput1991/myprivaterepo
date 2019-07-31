88a2be4f70b2c8393f3a3ad366d8e00cb1a31053
Rahul kumar <rahul.kumar@hpe.com>
2019-07-31 20:59:41 +0530

    Added express App with node js server

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+// step 1 create a express app
+const app=express();
+// step 2 an express app is a big chain of middleware
+app.use((req, resp, next) => {
+  {
+    console.log('First MW')
+    next();
+  }
+});
+app.use((req,resp,next) => {
+  {
+    console.log('second MW')
+    //next(); // so request wont travel down because we not calling next();
+    resp.send('Hellow from express')
+}})
+
+// step 3 now we have to wire up our express app with node js server written in server.js
+module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // We can actually excute any JS file with node except for files that try to access something in dom because node is server side run time and there is no dom there.
 const http = require('http');
 // This is not package which has anything to do with package.json. but this is installed by default once you installed node js.
-const server= http.createServer((req, resp) => {
-  resp.end('This is first response');
-});
+const app = require('./backend/app')
+const port = process.env.PORT || 3000;
+app.set('port', port);
+const server = http.createServer(app);
 // with that we created server but its not active yet.c
-server.listen(process.env.PORT || 3000);
+server.listen(port);
 // start the server just using node server.js on command line
+
+// --------------------------
+// Lets use express js framework for node js which makes node js developement easier.
+// install it using npm install --save express
