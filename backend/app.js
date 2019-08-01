7556dd087ff78e41dfc0f81a62db1e22193cdeb0
Rahul kumar <rahul.kumar@hpe.com>
2019-08-01 20:25:34 +0530

    Added route for /api/posts at server side logic with hardcoded data before we connect backend to angular

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,25 @@ const express=require('express');
 // step 1 create a express app
 const app=express();
 // step 2 an express app is a big chain of middleware
-app.use((req, resp, next) => {
-  {
-    console.log('First MW')
-    next();
-  }
-});
-app.use((req,resp,next) => {
+app.use('/api/posts',(req,resp,next) => {
   {
     console.log('second MW')
     //next(); // so request wont travel down because we not calling next();
-    resp.send('Hellow from express app')
+    //resp.send('Hellow from express app');
+    //later these posts can come from DB ofcourse
+    const posts = [
+      { id: '484n349nd', title: 'Firsts server side Post', content: 'This is coming from server' },
+      { id: '484n349nd', title: 'second server side Post', content: 'This is coming from server' }
+    ]
+    //resp.json(posts);
+    /// better u can send other object too
+     resp.status(200).json({
+      message: 'Posts fetched successfully',
+      posts:posts
+    }
+    )
+    //now u can access localhost:3000/api/posts and we will connect angular app to it using angular http client
+
 }})
 
 // step 3 now we have to wire up our express app with node js server written in server.js
--- src/mynotes.md
+++ src/mynotes.md
@@ -24,3 +24,8 @@ the rxjs.from the beginning.
 Rxjs is all about observables and this is a concept which can be a bit more complex to grasp,
 
 it's essentially about objects that help us pass data around
+----------------------------------------
+server side prograaming:
+1.Notice backend code and frontend code should be totally decoupled. it can be same model but different model also. May be u have different additonal fields on model at server side rather than what is present at client side.
+example: usermodel ,on backend you might store some extra information which you dont want to pass to client.
+2. at server side , we strict to de-facto of node which is js file rather than ts based model
