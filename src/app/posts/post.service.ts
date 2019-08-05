c9aa7f0f5f9c91c47b28cd49b1732595f133000f
Rahul kumar <rahul.kumar@hpe.com>
2019-08-05 22:31:17 +0530

    Added mapping of _id from server side to id at frontend side

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,7 +1,8 @@
 import { Post } from "./post.model";
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class PostService
@@ -24,9 +25,21 @@ export class PostService
     //subscribe takes 3 arg. first : new data , 2nd error and third for when it completes. For now ,lets focus only on data
     // Making get of type Post[] might be wrong because body of response at backend contains message and posts
     // since Post at backend has id as well, so we should add this add front end model as well.
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts').subscribe((postData) =>
+    this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+    .pipe(map((postData)=>
+    { return postData.posts.map(post=>{
+
+      return {
+     title: post.title,
+     content: post.content,
+     id: post._id
+      };
+    });
+    }
+      ))
+    .subscribe((transformposts) =>
     {
-      this.posts = postData.posts; //RHS is posts coming from server . Notice that no need to duplicate this as well because we cant accidently change
+      this.posts = transformposts; //RHS is posts coming from server . Notice that no need to duplicate this as well because we cant accidently change
       //it onserver , there is no such connrection, it is http response.
 
       //Now we need to inform our app for this update
