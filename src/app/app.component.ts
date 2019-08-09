7e8d69ba16c475267bcab61c5d4ed6a7fcdfe7a8
Rahul kumar <rahul.kumar@hpe.com>
2019-08-09 19:57:37 +0530

    calling getauthuser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from './posts/post.model';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authservice: AuthService)
+  {
+
+  }
+  ngOnInit(): void
+  {
+    this.authservice.getAuthUser(); // its auto auth user
+  }
   //title = 'mean-project';
 
   // storedPosts: Post[] = [];
@@ -16,4 +25,5 @@ export class AppComponent {
   //   //since array is of type Post now .. you cant add anything . so it enforces structure of Post
   //   this.storedPosts.push(post);
   // }
+
 }
