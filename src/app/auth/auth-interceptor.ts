e4f1c0d3be23d9f582523b98d848e045d55200c4
Rahul kumar <rahul.kumar@hpe.com>
2019-08-09 14:24:12 +0530

    Manipulated incoming req. with token

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -13,8 +13,15 @@ export class AuthInterceptor implements HttpInterceptor
   intercept(req: HttpRequest<any>,next: HttpHandler)
   { // next allows to leave the interceptor and allows other part of app or to continue this req journey in app
     // but we have to manimulate req. with token , so inject authservice first
-    const token = this.authservice.getToken();
+    const authtoken = this.authservice.getToken();
+    // notice we should clone req first to avoid any unwanted manipulation
+    const authRequest = req.clone({
+      // original header + append token
+      headers: req.headers.set('Authorization', "Bearer " + authtoken) // notice we using authorization header only at backend and splitting
+      //it based on whitspace
+    }); // will create copy of req and we can also pass configuration to clone
     return next.handle(req);
+
     // continue req journey
   }
 }
