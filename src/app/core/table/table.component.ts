5db62b642890641221138fbe64271ea04df4bc5e
Rahul kumar <rahul.kumar@hpe.com>
2020-03-02 19:25:54 +0530

    Refractoring

--- src/app/core/table/table.component.ts
+++ src/app/core/table/table.component.ts
@@ -35,19 +35,23 @@ export class TableComponent implements OnChanges, OnInit {
   }
 
   push() {
-    const table = document.getElementById('datatable') as HTMLTableElement;
+    const table = document.getElementById('datatable') as HTMLTableElement; // always return first
+    //const table = document.getElementsByTagName('datatable') as HTMLTableElement
     const rowCount = table.rows.length;
+    console.dir(table.rows);
     alert(rowCount);
     alert(this.settings.series.length);
+    console.dir(this.settings.series);
 
     for (let j = 0; j < this.settings.series.length; j++) {
-      const oCells = table.rows[j].cells;
+      const oCells = table.rows[j].cells; //here every time first table referend is there , you need to have unique id for each table.
       console.dir(oCells);
       const resource = {};
+      console.dir(this.settings.headers);
       this.settings.headers.forEach(function (value, i)
       {
 
-        console.log(value + "==" + i+"==="+ oCells.item(i + 1));
+        console.log(value + "==" + i+"==="+ oCells.item(i + 1).innerHTML);
         resource[value] = oCells.item(i + 1).innerHTML;
       });
       console.dir('Row Object is' + resource);
